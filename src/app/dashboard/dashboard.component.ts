2d250783263ea532caa37a8b5c4df345da850f3d
chrisybee88 <chrisybee88@gmail.com>
2025-01-06 12:23:18 +0000

    update CV

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,7 +14,7 @@ export class DashboardComponent implements OnInit {
     console.log("%c Checkout the source code of Chris Blackwell's CV !!", 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
     var code = prompt("Please enter your share code:");
-    var awesome = "Architect";
+    var awesome = "Security";
     var fun = "Rox";
     var d = new Date().getFullYear();
 
